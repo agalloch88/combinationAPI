a9a33c185e561c5d51dfb8ee8b9b3c489f74a7bd
Ryan Kirsch <agalloch88@gmail.com>
2023-01-15 12:57:28 -0500

    check currency, get deals from cheapshark, convert currency, and return repriced game deals

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -1,19 +1,60 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { formatJSONResponse } from "../../libs/apiGateway";
+import axios from "axios";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const { queryStringParameters = {} } = event;
+  try {
+    const { queryStringParameters = {} } = event;
 
-  const { currency } = queryStringParameters;
+    // check the currency, and ensure it is on the query string
 
-  if (!currency) {
-    return formatJSONResponse({
-      statusCode: 400,
-      data: {
-        message: "Missing currency query parameter. Currency is required.",
-      },
+    const { currency } = queryStringParameters;
+
+    if (!currency) {
+      return formatJSONResponse({
+        statusCode: 400,
+        data: {
+          message: "Missing currency query parameter. Currency is required.",
+        },
+      });
+    }
+
+    // get the deals from cheapshark, and convert the prices to the currency
+    const deals = await axios.get(
+      "www.cheapshark.com/api/1.0/deals?upperPrice=15&pageSize=5"
+    );
+
+    const currencyData = await axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${currency}.json`
+    );
+
+    const currencyConversion = currencyData.data[currency];
+
+    const repricedDeals = deals.data.map((deal) => {
+      const {
+        title,
+        storeID,
+        salePrice,
+        normalPrice,
+        metacriticScore,
+        savings,
+        steamRatingPercent,
+        releaseDate,
+      } = deal;
+
+      return {
+        title,
+        storeID,
+        steamRatingPercent,
+        metacriticScore,
+
+        salePrice: salePrice * currencyConversion,
+        normalPrice: normalPrice * currencyConversion,
+        savings: savings * currencyConversion,
+        releaseDate: new Date(releaseDate * 1000).toDateString(),
+      };
     });
-  }
 
-  return formatJSONResponse({ data: { message: "success" } });
+    return formatJSONResponse({ data: repricedDeals });
+  } catch (error) {}
 };
