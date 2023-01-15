8873c24114a1f38da418a259fe3c68abbc4514f7
Ryan Kirsch <agalloch88@gmail.com>
2023-01-15 12:15:48 -0500

    add parsing for query string parameter of currency

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -1,6 +1,20 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
+
+    const { queryStringParameters = {} } = event;
+
+    const { currency } = queryStringParameters;
+
+    if (!currency) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: "Missing Query String of currency",
+            }),
+        };
+    }
+
     return {
         statusCode: 200,
         body: JSON.stringify({
