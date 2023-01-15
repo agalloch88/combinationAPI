333c2aa2b2815dfe3581a2255703de87ee50d2cf
Ryan Kirsch <agalloch88@gmail.com>
2023-01-15 12:24:47 -0500

    use formatJSONResponse library function in combinationAPI returns

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -1,25 +1,19 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { formatJSONResponse } from "../../libs/apiGateway";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
+  const { queryStringParameters = {} } = event;
 
-    const { queryStringParameters = {} } = event;
+  const { currency } = queryStringParameters;
 
-    const { currency } = queryStringParameters;
+  if (!currency) {
+    return formatJSONResponse({
+      statusCode: 400,
+      data: {
+        message: "Missing currency query parameter. Currency is required.",
+      },
+    });
+  }
 
-    if (!currency) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: "Missing Query String of currency",
-            }),
-        };
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Hello from combinationAPI",
-            path: event.path,
-        }),
-    };
+  return formatJSONResponse({ data: { message: "success" } });
 };
